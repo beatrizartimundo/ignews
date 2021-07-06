5d8b668d4ca36552628eb30df0719e1e92351282
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-07-06 07:57:15 -0300

    aula inicial e configuracoes

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import  Head  from 'next/head'
+import styles from '../styles/home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>ig.news</title>
+    </Head>
+      <h1 className={styles.title}>Oie</h1>
+    </>
+  )
+}
