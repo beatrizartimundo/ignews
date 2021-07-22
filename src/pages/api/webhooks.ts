81c213b787a57e983e46c3b89e3af72dca77e229
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-07-22 08:13:15 -0300

    add prismic config

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,7 +22,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.deleted'
 ])
 export default async(req: NextApiRequest, res: NextApiResponse)=> {
     if(req.method === 'POST'){
@@ -43,12 +45,24 @@ export default async(req: NextApiRequest, res: NextApiResponse)=> {
             try{
                 //caso o stripe retorne a resposta mas o hook de algum problema
                 switch(type){
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        );
+
+                        break;
                     case 'checkout.session.completed':
                         const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                     )
 
                     break;
