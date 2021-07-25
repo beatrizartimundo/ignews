e40aed322956052add5feb3e862f929162e918fe
beatrizartimundo <beatriz.artimundo@gmail.com>
2021-07-25 09:32:39 -0300

    add links configuration (cloneElement)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { AcitveLink } from '../ActiveLink';
 
 export function Header(){
+   
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-
+                    <AcitveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </AcitveLink>
+                    <AcitveLink activeClassName={styles.active} href="/posts">
+                      <a>Posts</a>  
+                    </AcitveLink>
                 </nav>
                 <SignInButton/>
             </div>
